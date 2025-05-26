0af3f8011e6ed19454af869761aa5747004f3319
kekeanu <klumando_240000000910@uic.edu.ph>
2025-05-26 16:20:38 +0800

    Add files via upload

---
+++ TLA5/src/main/java/com/mycompany/tla5/TLA5.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.tla5;
+
+/**
+ *
+ * @author Student's Account
+ */
+public class TLA5 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
